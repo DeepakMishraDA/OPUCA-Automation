8cf83aa5cbd53b316f4a62a19b95022fa2caa509
deepak mishra <d.mishra@green-fusion.de>
2022-07-07 22:06:38 +0200

    client two added

---
+++ src/client2opcua.js
@@ -0,0 +1,15 @@
+const {OPCUAClient} = require('node-opcua');
+require('dotenv').config();
+
+async function clientObject() {
+    const client = OPCUAClient.create({
+        endpointMustExist: false
+});
+     client.connect(process.env.ENDPOINT_URL);
+    client.on("connected", ()=>{
+        console.log("Client has connected!")
+    })
+    
+    const session = await client.createSession();
+   
+}
