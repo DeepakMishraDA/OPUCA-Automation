7cf01e47f16803c9b2d3759d25978adfc931c098
deepak mishra <d.mishra@green-fusion.de>
2022-07-08 00:02:00 +0200

    client callback added

--- src/client2opcua.js
+++ src/client2opcua.js
@@ -1,6 +1,8 @@
 const {OPCUAClient} = require('node-opcua');
 require('dotenv').config();
 
+const installAlarmMonitoring = require('../alarm_monitor/installing_alarms')
+
 async function clientObject() {
     const client = OPCUAClient.create({
         endpointMustExist: false
@@ -9,12 +11,29 @@ async function clientObject() {
     client.on("connected", ()=>{
         console.log("Client has connected!")
     })
-    
-    //const session = await client.createSession();
+    var the_session; 
+   client.createSession(async (err, session)=> {
+         session.read({nodeId: "ns=4;s=.S01.TLSpo.r_Messwert",
+        attributeId: AttributeIds.Value},
+        (err, data)=> {
+            if (!err){
+              console.log("This is it",data.value.value)
+            }
+        }) 
+      });
+   
+    // const subscription = await session.createSubscription2({
+    //     requestedPublishingInterval: 200,
+    //   requestedMaxKeepAliveCount: 20,
+    //   maxNotificationsPerPublish: 1000,
+    //   publishingEnabled: true,
+    //   priority: 10,
+    // });
+    // subscription.then(data=>{
+    //     console.log(data)
+    // })
 }
 
 clientObject().then(data => {
     console.log("this",data);
-}).catch(err=>{
-    console.log("Some",err)
 })
